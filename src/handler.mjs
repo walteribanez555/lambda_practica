9cbeb701614f97ea525bea2138c05f463c96a01b
walteribanez555 <walteribanez555@gmail.com>
2024-06-12 14:49:10 -0400

    query path data

--- src/handler.mjs
+++ src/handler.mjs
@@ -1,7 +1,16 @@
 export const handler = async (event) => {
+    console.log( 'Main Fecha-Hora: ', new Date() );
+    console.log( 'EVENT: ' , event );
+    const { method, path } = event?.requestContext?.http ? event.requestContext.http : {};
+    const { id } = typeof( event.queryStringParameters ) === 'object' && Object.keys( event.queryStringParameters ).length > 0 ? event.queryStringParameters : false;
+    const data = typeof( event.body ) === 'string' && Object.keys( parseJsonToObject( event.body ) ).length > 0 ? parseJsonToObject( event.body ) : {};
+    console.log( 'DATA: ' , data );
+    console.log( 'ID: ' , id );
+    console.log( 'METHOD: ' , method.toLowerCase() );
+    console.log( 'PATH: ' , path );
     // Your Lambda function code
     return {
         statusCode: 200,
-        body: JSON.stringify('Segunda prueba'),
+        body: JSON.stringify('Segunda prueba' + path),
     };
 };
