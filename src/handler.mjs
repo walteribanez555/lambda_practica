8888bf3e8b9c57198e2b7ea2172fe1ff0f5e9605
walteribanez555 <walteribanez555@gmail.com>
2024-06-12 15:26:44 -0400

    query path data

--- src/handler.mjs
+++ src/handler.mjs
@@ -11,6 +11,6 @@ export const handler = async (event) => {
     // Your Lambda function code
     return {
         statusCode: 200,
-        body: JSON.stringify('Segunda prueba' + path),
+        body: JSON.stringify('Segunda prueba' + path + ' ' + method.toLowerCase() + ' ' + id),
     };
 };
