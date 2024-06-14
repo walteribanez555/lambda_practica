d5114a09a864e17e6b88bd30123e8feaf44ed324
walteribanez555 <walteribanez555@gmail.com>
2024-06-14 11:54:38 -0400

    add details

--- src/handler.mjs
+++ src/handler.mjs
@@ -10,7 +10,9 @@ export const handler = async (event) => {
     console.log( 'Main Fecha-Hora: ', new Date() );
     console.log( 'EVENT: ' , event );
     const { method, path } = event?.requestContext?.http ? event.requestContext.http : {};
-    const { id, init, end, nro_identificacion  } = typeof( event.queryStringParameters ) === 'object' && Object.keys( event.queryStringParameters ).length > 0 ? event.queryStringParameters : false;
+    // const authorization = event?.headers?.authorization ? event.headers.authorization : false;
+    // const schema = event.headers.schema || 'assist_trip';
+    const { id, init, end, nro_identificacion } = typeof( event.queryStringParameters ) === 'object' && Object.keys( event.queryStringParameters ).length > 0 ? event.queryStringParameters : false;
     const data = typeof( event.body ) === 'string' && Object.keys( parseJsonToObject( event.body ) ).length > 0 ? parseJsonToObject( event.body ) : {};
     console.log( 'DATA: ' , data );
     console.log( 'ID: ' , id );
