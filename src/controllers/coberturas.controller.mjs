3ee613da6d82c9ddf71c02500244f152fed9981f
walteribanez555 <walteribanez555@gmail.com>
2024-06-14 14:05:47 -0400

    Add Coberturas Files

--- src/controllers/coberturas.controller.mjs
+++ src/controllers/coberturas.controller.mjs
@@ -1,15 +1,11 @@
+import { getPlanes } from "../models/coberturas.model.mjs";
 import { buildResponse } from "../utils/helpers.mjs";
 
 
 
 export async function getCoberturas({ id, schema }){
-
-    try{
-        return buildResponse(200, 'respuesta', 'get');
-    }catch( error ){
-        return buildResponse(500, error, 'get');
-    }
-
+    // return getPlanes({ id, schema : 'redcard' });
+    return getPlanes({ id, schema : 'redcard' })
 }
 
 
