27eabe878c00ed4d6e5904c6421228bbd26a2280
walteribanez555 <walteribanez555@gmail.com>
2024-06-14 16:27:18 -0400

    Controller reinit

--- src/controllers/coberturas.controller.mjs
+++ src/controllers/coberturas.controller.mjs
@@ -4,12 +4,12 @@ import { buildResponse } from "../utils/helpers.mjs";
 
 
 export async function getCoberturas({ id}){
-    // return getPlanes({ id, schema : 'redcard' });
-    try{
-        return buildResponse(200, 'realizado', 'get');
-    }catch{
-        return buildResponse(500, error, 'get');
-    }
+    return getPlanes({ id, schema : 'redcard' });
+    // try{
+    //     return buildResponse(200, 'realizado', 'get');
+    // }catch{
+    //     return buildResponse(500, error, 'get');
+    // }
 }
 
 
