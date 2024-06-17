6f70c4c20d18f2824e897b03cfce271f57865e90
walteribanez555 <walteribanez555@gmail.com>
2024-06-17 13:54:04 -0400

    PostVenta Detail Created

--- src/controllers/ventas.controller.mjs
+++ src/controllers/ventas.controller.mjs
@@ -5,3 +5,13 @@ import { buildResponse } from "../utils/helpers.mjs";
 export async function getVentas({id}){
     return await getPlanes({ id, schema : 'redcard'});
 }
+
+
+
+export async function postVenta({ data  } ) { 
+    if( !data.fecha_salida || !data.fecha_retorno || !data.destiny || !vouchers  ){
+        return buildResponse(500, {message : "Missing data"}, 'post');
+    }
+
+    return buildResponse(200, {message : "Venta creada"}, 'post');
+}
