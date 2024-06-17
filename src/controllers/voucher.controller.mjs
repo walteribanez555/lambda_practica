8375cda56f6927ca199e12a85b5b323737bc0941
walteribanez555 <walteribanez555@gmail.com>
2024-06-17 11:21:31 -0400

    Voucher number required error handler

--- src/controllers/voucher.controller.mjs
+++ src/controllers/voucher.controller.mjs
@@ -2,7 +2,7 @@ import { getPolizas } from "../models/polizas.model.mjs";
 
 export async function getVoucher({ id }) {
   if (!id) {
-    return buildResponse(500, "voucher number required", "get");
+    return buildResponse(500, {message : 'number voucher required'}, 'get');
   }
 
   return await getPolizas({ id, schema: "redcard" });
