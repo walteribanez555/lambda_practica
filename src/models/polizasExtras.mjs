bac1f349a3756d8efae44404bbb84cb8f3144f89
walteribanez555 <walteribanez555@gmail.com>
2024-07-15 11:20:09 -0400

    Venta Controller Logic Updated

--- src/models/polizasExtras.mjs
+++ src/models/polizasExtras.mjs
@@ -35,19 +35,22 @@ export async function postPolizasExtras( { data, schema } ) {
         const newRegister = validateData( data, model );
         if ( Object.keys( newRegister ).length === 0 )
             return buildResponse( 400, { message : 'Missing required fields or not valid' }, 'post' );
+        
 
-        const beneficios = newRegister.beneficio_id.split(',');
-        const montos = newRegister.monto_adicional.split(',');
-        const result = [];
-        for ( let i = 0; i < beneficios.length; i++ ) {
-            const register = {
-                venta_id : newRegister.venta_id,
-                beneficio_id : beneficios[ i ],
-                monto_adicional : montos[ i ],
-            };
-            const response = await database.create( register, keyField );
-            result.push( response[ 0 ] );
-        }
+        const result = await database.create( newRegister, keyField );
+
+        // const beneficios = newRegister.beneficio_id.split(',');
+        // const montos = newRegister.monto_adicional.split(',');
+        // const result = [];
+        // for ( let i = 0; i < beneficios.length; i++ ) {
+        //     const register = {
+        //         venta_id : newRegister.venta_id,
+        //         beneficio_id : beneficios[ i ],
+        //         monto_adicional : montos[ i ],
+        //     };
+        //     const response = await database.create( register, keyField );
+        //     result.push( response[ 0 ] );
+        // }
         return buildResponse( 200, result, 'post', keyField, data );
     
     } catch ( error ) {
